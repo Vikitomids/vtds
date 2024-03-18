5d93a3fbfc0ff85ba872fa177cb763fa57f9fd56
vikitomi <vikitomi90@gmail.com>
2024-03-18 11:04:40 +0700

    ojo dibukak

---
+++ ojo_dibukak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vtds;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class ojo_dibukak {
+public static void main(String[] args) {
+ System.out.println("SELAMAT ANDA MENDAPATKAN 2 MOBIL KIJANG DAN 1 KUPON PAKET UMROH");
+  }
+}
+
